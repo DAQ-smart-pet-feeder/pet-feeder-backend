9b9e59d6495516ac7ec64457a80e94dc03476d22
JiratchayaPhinyodom <jiratchaya.phiny@ku.th>
2023-11-23 17:38:37 +0700

    Make more 3 path that fronend can get he portion data including timestamp. And also have 2 path which are the path that can recieve and send tankData.

--- controller.py
+++ controller.py
@@ -36,29 +36,26 @@ def get_sensor_tank_data():
         result = [models.SensorTankData(*row) for row in cs.fetchall()]
         return result
 
+def get_tank_data():
+    with pool.connection() as conn, conn.cursor() as cs:
+        cs.execute("""
+            SELECT remaining_percentage, feeding_status
+            FROM tankData
+            ORDER BY id DESC
+            LIMIT 1
+        """)
+        result = [{'remaining_percentage': int(row[0]), 'feeding_status': int(row[1])} for row in cs.fetchall()]
+        return result
+
    
 def get_feeding_data():
     with pool.connection() as conn, conn.cursor() as cs:
         cs.execute("""
-            SELECT ts, dis, por
+            SELECT id, ts, por
             FROM feedingData
         """)
         result = [models.FeedingData(*row) for row in cs.fetchall()]
         return result
-
-
-def get_feeding_details(id):
-    with pool.connection() as conn, conn.cursor() as cs:
-        cs.execute("""
-            SELECT ts, por
-            FROM feedingData
-            WHERE id=%s
-            """, [id])
-        result = cs.fetchone()
-    if result:
-        return models.FeedingData(*result)
-    else:
-        abort(404)
 
 
 def get_behavior_data():
@@ -99,6 +96,25 @@ def post_portion_data():
             conn.commit()
         # Return success message or relevant data
         return {'message': 'Data inserted successfully'}, 201
+
+    except Exception as e:
+        # Handle errors appropriately
+        print(f"Error: {e}")
+        return {'error': 'Internal Server Error'}, 500
+
+def post_tank_data():
+    try:
+        with pool.connection() as conn, conn.cursor() as cs:
+            request_data = request.get_json() 
+            print(request_data)
+            insert_query = "INSERT INTO tankData (remaining_percentage, feeding_status) VALUES (%s, %s)"
+            cs.execute(insert_query, (request_data['remaining_percentage'], request_data['feeding_status']))
+
+            # Commit changes and close the database connection
+            conn.commit()
+
+            # Return success message or relevant data
+            return {'message': 'Data inserted successfully'}, 201
 
     except Exception as e:
         # Handle errors appropriately
